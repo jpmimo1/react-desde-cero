d4f1c02c96b664acd3bf990f7134b111122de228
Jean Paul <paul.fa.ac@gmail.com>
2020-06-29 23:34:22 -0500

    Primer commit del curso de React.js desde cero

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/styles.scss";
+import Curso from "./curso";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img
+            className="main-banner__img"
+            src="https://images2.alphacoders.com/106/1063352.jpg"
+            alt="banner"
+          />
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Título del banner</p>
+            <p>Subtítulo del banner</p>
+            <a href="http://google.com" className="button">
+              Botón del banner
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
+
+    <div className="ed-grid m-grid-3">
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+    </div>
+  </>
+);
 
 export default App;
